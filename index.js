b8c6d8e3a59a2d68ebeea9035086d9bddb6ff308
Steven <stevendawn98@gmail.com>
2022-05-22 18:30:58 -0400

    did scope lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+    return customerName;
+  }
+
+    var bestCustomer;
+function setBestCustomer() {
+    bestCustomer = 'not bob'
+    return bestCustomer;
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+    return bestCustomer;
+}
+    const leastFavoriteCustomer = 'billy'
+function changeLeastFavoriteCustomer() {
+     leastFavoriteCustomer = 'billy bob'
+     return leastFavoriteCustomer;
+}
